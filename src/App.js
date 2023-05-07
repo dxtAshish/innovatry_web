53d554d6564d0dc26a446cbbdffde69a6678bdd7
Rishabh <rishabhgoswami901@gmail.com>
2023-05-07 14:05:37 +0530

    app.js routes

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ import Signup from './screens/Signup';
 import Profile from './screens/Profile';
 import Buy from './component/Buy';
 import Sell from './component/Sell';
+import Checkout from './component/Checkout';
+import ProductDetail from './component/ProductDetail';
+import Orders from './screens/Orders';
 export default function App() {
   return (
     <>
@@ -16,12 +19,16 @@ export default function App() {
     <Routes>
      
       <Route exact path='/' element={<Home/>}/>
+      <Route exact path='/:category' element={<Home/>}/>
       <Route exact path='/Login' element={<Login/>}/>
       <Route exact path='/Signup' element={<Signup/>}/>
       <Route exact path='/profile' element={<Profile/>}/>
-      <Route exact path='/Buy' element={<Buy/>}/>
-      <Route exact path='/Buy/:product_id' element={<Buy/>}/>
+      <Route exact path='/Buy' element={<ProductDetail/>}/>
+      <Route exact path='/Buy/:product_id' element={<ProductDetail/>}/>
       <Route exact path='/Sell' element={<Sell/>}/>
+      <Route exact path='/checkout' element={<Checkout/>}/>
+      <Route exact path='/profile/:profile_url' element={<Profile/>}/>
+      
     </Routes>
   </div>
 </Router>
